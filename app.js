11f210c8ff49d9a1581d419bac819e0dedccf853
erkansevim <erkansevim.essgmail.com>
2024-05-24 09:34:18 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let names=['erkan', 'betül', 'harvey', 'ashly'];
+
+console.log(typeof(names));
+console.log(typeof(names[0]));
+console.log(names.length);
+console.log(Boolean(names));
+
+names.length ? console.log("there is data"):'there is no data';
+//---------
+for (let i=0; i<names.length;i++){
+    console.log(names[i]);
+}
+
+
+//-------
+let counter=0
+while (counter<names.length){
+    console.log(names[counter]);
+    counter++
+}
