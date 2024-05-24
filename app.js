d44d83ea9901526177153b27caca4238f95f0cde
erkansevim <erkansevim.essgmail.com>
2024-05-24 10:10:43 +0200

    findVowels

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-let names=['erkan', 'betül', 'harvey', 'ashly'];
+// let names=['erkan', 'betül', 'harvey', 'ashly'];
 
-console.log(typeof(names));
-console.log(typeof(names[0]));
-console.log(names.length);
-console.log(Boolean(names));
+// console.log(typeof(names));
+// console.log(typeof(names[0]));
+// console.log(names.length);
+// console.log(Boolean(names));
 
-names.length ? console.log("there is data"):'there is no data';
-//---------
-for (let i=0; i<names.length;i++){
-    console.log(names[i]);
+// names.length ? console.log("there is data"):'there is no data';
+// //---------
+// for (let i=0; i<names.length;i++){
+//     console.log(names[i]);
+// }
+
+
+// //-------
+// let counter=0
+// while (counter<names.length){
+//     console.log(names[counter]);
+//     counter++
+// }
+
+
+// //---------
+
+// names.forEach(i=>console.log(i))
+
+// //---------
+
+
+let ifade="ben felegin tekerine";
+
+function findVowels(par) {
+    let vowels="aeiou";
+    let newStr="";
+
+    for (let i = 0; i < par.length; i++) {
+       
+        if (vowels.includes(par[i])) {
+            newStr+=par[i];
+        }
+       
+    }
+    return newStr
 }
 
-
-//-------
-let counter=0
-while (counter<names.length){
-    console.log(names[counter]);
-    counter++
-}
-
-
-//---------
-
-names.forEach(i=>console.log(i))
+console.log(findVowels(ifade));
